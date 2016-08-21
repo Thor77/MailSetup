112684b79af6c556755a5359ebec5312cb0e83b1
Thor77 <thor77@thor77.org>
2016-08-21 22:52:00 +0100

    Use ask to query for password instead of cli-arg
    * circumvent problems with escaping in some hash-formats

--- cli/user.rb
+++ cli/user.rb
@@ -4,8 +4,9 @@ require './model'
 module MailmgrCLI
   # Subclass User to manage virtual users
   class User < Thor
-    desc 'add <name> <password> <domain>', 'Add a new user'
-    def add(name, password, domain)
+    desc 'add <name> <domain>', 'Add a new user'
+    def add(name, domain)
+      password = ask('Password:', echo: false)
       raise 'Password too long!' if password.length > 120
       domain = Vdomain.first name: domain
       user = Vuser.new name: name, vdomain: domain, password: password
